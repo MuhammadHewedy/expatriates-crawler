730b84671bd627d3e30e482c7aa2ebab56220915
Muhammad Hewedy <mhewedy@gmail.com>
2016-08-23 00:53:50 +0300

    overcome server issues and tie with mongo

--- app.js
+++ app.js
@@ -1,11 +1,20 @@
 var express = require('express');
 var inspector = require('./inspector.js');
+var MongoClient = require('mongodb').MongoClient;
 
 var app = express();
 var domain = "https://www.expatriates.com/";
+var mongoUrl = 'mongodb://localhost:27017/expatriates';
+
 
 app.get('/', function(req, res) {
-    inspector.call(domain, parseHome);
+
+    MongoClient.connect(mongoUrl, function(err, db) {
+        inspector.call(domain, parseHome, {
+            mongoDb: db
+        });
+        // TODO close db
+    });
     res.send("OK");
 });
 
@@ -13,29 +22,35 @@ app.listen(3000, function() {
     console.log('Example app listening on port 3000!');
 });
 
-function parseHome($) {
+function parseHome($, cbParams) {
     var lists = $('.categories li a').map(function(a) {
         return $(this).attr('href');
     });
     console.log('found ' + lists.length + ' lists');
     lists.each(function(i, listUrl) {
-        inspector.call(domain + listUrl, parseList);
+        inspector.call(domain + listUrl, parseList, cbParams);
 
         // TODO handle paging
         // listUrl + n * (100) where n = 1 until we got zero results
     });
 }
 
-function parseList($) {
+function parseList($, cbParams) {
     var posts = $('.main-content li > a').map(function(a) {
         return $(this).attr('href');
     });
     console.log('found ' + posts.length + ' posts');
     posts.each(function(i, postUrl) {
-        inspector.call(domain + postUrl, parsePost);
+        inspector.call(domain + postUrl, parsePost, cbParams);
     });
 }
 
-function parsePost($) {
-    // TODO
+function parsePost($, cbParams) {
+    var title = $('title').text();
+    var db = cbParams.mongoDb;
+
+    var titlesCollect = db.collection('titles');
+    titlesCollect.insertOne({
+        title: title
+    });
 }
--- inspector.js
+++ inspector.js
@@ -1,14 +1,27 @@
 var cheerio = require('cheerio');
 var request = require('request');
 
-module.exports.call = function(url, callback) {
-    request(url, function(error, response, html) {
+var sCount = 0,
+    eCount = 0;
+
+var call = function(url, callback, cbParams) {
+    request({
+        "headers": {
+            "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/52.0.2743.116 Safari/537.36"
+        },
+        "url": url
+    }, function(error, response, html) {
         if (!error) {
             var $ = cheerio.load(html);
-            console.log(html);
-            callback($);
+            callback($, cbParams);
+            sCount++;
         } else {
-            console.error(url, response.statusCode, error);
+            // console.error(url, error.code);
+            // eCount++;
+            call(url, callback, cbParams);
         }
+        console.log(sCount, eCount);
     });
 };
+
+module.exports.call = call;
