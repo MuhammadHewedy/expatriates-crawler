2162fa103179053ea4e633290f35ee1faeea158e
Muhammad Hewedy <mhewedy@gmail.com>
2016-08-24 23:41:54 +0300

    convert the express app to cli app

--- app.js
+++ app.js
@@ -1,23 +1,16 @@
-var express = require('express');
+#! /usr/bin/env node
+
 var inspector = require('./inspector.js');
 var MongoClient = require('mongodb').MongoClient;
 
-var app = express();
 var domain = "https://www.expatriates.com/";
 var mongoUrl = 'mongodb://localhost:27017/expatriates';
 
-
-app.get('/', function(req, res) {
-
-    MongoClient.connect(mongoUrl, function(err, db) {
-        inspector.call(domain, parseHome, {mongoDb: db});
-        // TODO close db
+MongoClient.connect(mongoUrl, function(err, db) {
+    inspector.call(domain, parseHome, {
+        mongoDb: db
     });
-    res.send("OK");
-});
-
-app.listen(3000, function() {
-    console.log('Example app listening on port 3000!');
+    // TODO close db
 });
 
 function parseHome($, cbParams) {
@@ -26,7 +19,7 @@ function parseHome($, cbParams) {
     });
     console.log('found ' + lists.length + ' lists');
     lists.each(function(i, listUrl) {
-		cbParams.listUrl = listUrl;
+        cbParams.listUrl = listUrl;
         inspector.call(domain + listUrl, parseList, cbParams);
     });
 }
@@ -39,17 +32,17 @@ function parseList($, cbParams) {
     posts.each(function(i, postUrl) {
         inspector.call(domain + postUrl, parsePost, cbParams);
     });
-	
-	// paging
-	if (($('.pagination a.active').text() || 0) == 1){	// in the landing page of the list
-		var toPage = $('.pagination a:nth-last-child(2)').text() || 0;
-		if (toPage > 1){
-			console.log('found ' + toPage + ' pages for list ' + cbParams.listUrl);
-			for (var i = 1; i < toPage; i++){
-				inspector.call(domain + cbParams.listUrl + "/index" + i * 100 + ".html", parseList, cbParams);
-			}
-		}
-	}
+
+    // paging
+    if (($('.pagination a.active').text() || 0) == 1) { // in the landing page of the list
+        var toPage = $('.pagination a:nth-last-child(2)').text() || 0;
+        if (toPage > 1) {
+            console.log('found ' + toPage + ' pages for list ' + cbParams.listUrl);
+            for (var i = 1; i < toPage; i++) {
+                inspector.call(domain + cbParams.listUrl + "/index" + i * 100 + ".html", parseList, cbParams);
+            }
+        }
+    }
 }
 
 function parsePost($, cbParams) {
